1fe30a77a98f5adcb0c519d5b11eedb461bb7723
Cody Condon <codycondon50@gmail.com>
2016-04-06 12:16:29 -0400

    rendering first movie from data

--- index.ios.js
+++ index.ios.js
@@ -31,21 +31,49 @@ class ToDo extends Component {
     componentDidMount(){
         this.fetchData();
     }
-  render() {
-      var movie = MOCKED_MOVIES_DATA[0];
-    return (
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    }
+    render() {
+        if (!this.state.movies) {
+        return this.renderLoadingView();
+        }
+
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    }
+
+    renderLoadingView() {
+        return (
         <View style={styles.container}>
-            <Image 
+            <Text>
+            Loading movies...
+            </Text>
+        </View>
+        );
+    }
+
+    renderMovie(movie) {
+        return (
+        <View style={styles.container}>
+            <Image
             source={{uri: movie.posters.thumbnail}}
             style={styles.thumbnail}
             />
             <View style={styles.rightContainer}>
-                <Text style={styles.title}>{movie.title}</Text>
-                <Text style={styles.year}>{movie.year}</Text>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
             </View>
         </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
