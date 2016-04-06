e3e4647ec7d4d25415659ca2d3a6ee97adee572a
Cody Condon <codycondon50@gmail.com>
2016-04-06 12:07:45 -0400

    add in api and constructor

--- index.ios.js
+++ index.ios.js
@@ -12,20 +12,37 @@ import React, {
   View
 } from 'react-native'; 
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var MOCKED_MOVIES_DATA = [ 
     {title: 'Title',
      year: '2015',
       posters: {
-        thumbnail: 'http://i.imgur.com/UePbdph.jpg' 
-    }};
+      thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
 class ToDo extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            movies: null,
+        };
+    }
+    componentDidMount(){
+        this.fetchData();
+    }
   render() {
       var movie = MOCKED_MOVIES_DATA[0];
     return (
         <View style={styles.container}>
-            <Text>{movie.title}</Text>
+            <Image 
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}
+            />
+            <View style={styles.rightContainer}>
+                <Text style={styles.title}>{movie.title}</Text>
+                <Text style={styles.year}>{movie.year}</Text>
+            </View>
         </View>
     );
   }
@@ -34,19 +51,25 @@ class ToDo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+      width: 53, 
+      height: 81,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  rightContainer: {
+      flex: 1,
+  },
+  title:{ 
+      fontSize: 20,
+      marginBottom: 8,
+      textAlign: 'center',
+  }, 
+  year: {
+      textAlign: 'center',
   },
 });
 
